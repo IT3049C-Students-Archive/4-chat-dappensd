c2ed32480918a13efd7030502bd2ec887ba80e91
Samuel Dappen <60232895+dappensd@users.noreply.github.com>
2021-06-09 15:28:20 -0400

    Complete4d Getting Messages

---
+++ resources/js/index.js
@@ -0,0 +1,58 @@
+const nameInput = document.getElementById("my-name-input");
+const myMessage = document.getElementById("myMessage");
+const sendButton = document.getElementById("sendButton");
+const chatBox = document.getElementById("chat");
+const MILLISECONDS_IN_TEN_SECONDS = 10000;
+setInterval(updateMessages, MILLISECONDS_IN_TEN_SECONDS);
+
+async function updateMessages() {
+  // Fetch Messages
+  const messages = await fetchMessages();
+
+  // Loop over the messages. Inside the loop we will
+      // get each message
+      // format it
+      // add it to the chatbox
+    let formattedMessages = "";
+    messages.forEach(message => {
+      formattedMessages += formatMessage(message, nameInput.value);
+    });
+    chatBox.innerHTML = formattedMessages;
+
+}
+
+const serverURL = `https://it3049c-chat-application.herokuapp.com/messages`;
+
+function fetchMessages() {
+    return fetch(serverURL)
+        .then( response => response.json())
+}
+
+function formatMessage(message, myNameInput) {
+  const time = new Date(message.timestamp);
+  const formattedTime = `${time.getHours()}:${time.getMinutes()}`;
+
+  if (myNameInput === message.sender) {
+      return `
+      <div class="mine messages">
+          <div class="message">
+              ${message.text}
+          </div>
+          <div class="sender-info">
+              ${formattedTime}
+          </div>
+      </div>
+      `
+  } else {
+      return `
+          <div class="yours messages">
+              <div class="message">
+                  ${message.text}
+              </div>
+              <div class="sender-info">
+                  ${message.sender} ${formattedTime}
+              </div>
+          </div>
+      `
+  }
+}
